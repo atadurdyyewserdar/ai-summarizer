4d43b3946459b2d1f1434c69714208bb09e862da
Serdar <44369230+atadurdyyewserdar@users.noreply.github.com>
2025-11-24 17:15:36 +0100

    Removed unnessecary vode

--- ai-summarizer-backend/src/main/java/com/aissummarizer/jennet/security/JwtService.java
+++ ai-summarizer-backend/src/main/java/com/aissummarizer/jennet/security/JwtService.java
@@ -66,18 +66,6 @@ public class JwtService {
         return extractAllClaims(token).getExpiration().before(new Date());
     }
 
-//    public String generate(String username) {
-//        Date now = new Date();
-//        Date expiry = new Date(now.getTime() + expirationMs);
-//
-//        return Jwts.builder()
-//                .subject(username)
-//                .issuedAt(now)
-//                .expiration(expiry)
-//                .signWith(signingKey)
-//                .compact();
-//    }
-
     public String extractUsername(String token) {
         return Jwts.parser()
                 .verifyWith(signingKey)
@@ -86,15 +74,6 @@ public class JwtService {
                 .getPayload()
                 .getSubject();
     }
-
-//    public boolean isValid(String token, String expectedUsername) {
-//        try {
-//            String actual = extractUsername(token);
-//            return actual.equals(expectedUsername);
-//        } catch (Exception ignored) {
-//            return false;
-//        }
-//    }
 
     public boolean isTokenValid(String token, String expectedUsername) {
         return expectedUsername.equals(extractUsername(token)) && !isExpired(token);
